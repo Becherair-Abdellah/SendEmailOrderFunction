9fdcab928cb025eaf7d0f7cf3009ddcab8839cde
abdellah <abdeallahdz2004@gmail.com>
2026-01-08 23:07:57 +0100

    update sendgrid configuration and add real email

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ module.exports = async function (req, res) {
 
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+
     const msg = {
-      to: 'your-email@example.com', // your email
-      from: 'store@example.com',     // verified sender in SendGrid
+      to: 'abdeallahdz2004@gmail.com', // your email
+      from: 'abdellah.becherair@gmail.com',     // verified sender in SendGrid
       subject: `New Order #${order.$id}`,
       html: `
         <h2>New Order Details</h2>
-        <p><strong>Customer:</strong> ${order.customerName}</p>
-        <p><strong>Email:</strong> ${order.customerEmail}</p>
-        <p><strong>Total:</strong> ${order.total}</p>
-        <p><strong>Products:</strong> ${JSON.stringify(order.products)}</p>
+        <p><strong>Customer:</strong> ${order.fullname}</p>
+        <p><strong>phone:</strong> ${order.phone}</p>
+        <p><strong>Total:</strong> ${order.total_price}</p>
       `
     };
 
